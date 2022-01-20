4534a7c354a14c42a912bbdfee07d1f68157f7e4
mertkursun <mustafamertkursun@hotmail.com.tr>
2022-01-20 18:06:01 +0300

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const performance = window.performance;
-const API = 'https://mmk-perf-api.herokuapp.com/metrics'
+const API = 'https://mmk-perf-api.herokuapp.com/measures'
 
 const convertTime = (val) => {
   return (val / 1000)
